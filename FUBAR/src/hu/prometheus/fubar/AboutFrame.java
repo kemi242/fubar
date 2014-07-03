16aac2c77d646559e07d854f43cda368f53c50cb
kemi242 <kemi242spam@gmail.com>
2014-07-03 19:38:58 +0200

    Egyszerű: kötőjelek #10

--- FUBAR/src/hu/prometheus/fubar/AboutFrame.java
+++ FUBAR/src/hu/prometheus/fubar/AboutFrame.java
@@ -52,11 +52,12 @@ public class AboutFrame extends JFrame {
 		contentPane.setLayout(null);
 		
 		txtrInfo = new JTextArea();
+		txtrInfo.setTabSize(2);
 		txtrInfo.setEditable(false);
 		txtrInfo.setWrapStyleWord(true);
 		txtrInfo.setLineWrap(true);
 		txtrInfo.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
-		txtrInfo.setText("- Ez a j\u00E1t\u00E9k a \u201Efelelsz vagy mersz\u201D, az \u00FCvegez\u00E9s \u00E9s b\u00E1rmely pi\u00E1l\u00F3s j\u00E1t\u00E9k egy k\u00E1rtyaj\u00E1t\u00E9kba gy\u00FArva. \u00DCljetek k\u00F6rbe, sorban h\u00FAzzatok k\u00E1rty\u00E1t \u00E9s k\u00F6vess\u00E9tek az utas\u00EDt\u00E1sokat!\r\n\r\n- A j\u00E1t\u00E9k addig tart, am\u00EDg a piak\u00E9szlet. Az italt egys\u00E9gekben m\u00E9ritek. Ez lehet 1 dl s\u00F6r, egy korty bor  \u00E9s\\vagy 2 cl t\u00F6m\u00E9ny, a pia fajt\u00E1j\u00E1t szabadon ki lehet v\u00E1lasztani csak ne keverj\u00E9tek!\r\n\r\n- Minden k\u00E1rtya, amit h\u00FAztok, egy feladatot jelent. Ez lehet iv\u00E1szat, k\u00E9rdezz-felelek vagy ak\u00E1rmi. Aki nem teljes\u00EDti, b\u00FCntet\u0151t kap (4 cl p\u00E1linka 10 m\u00E1sodpercig a sz\u00E1jban, esetleg egy ruha lev\u00E9tele).");
+		txtrInfo.setText("\tEz a j\u00E1t\u00E9k a \u201Efelelsz vagy mersz\u201D, az \u00FCvegez\u00E9s \u00E9s b\u00E1rmely pi\u00E1l\u00F3s j\u00E1t\u00E9k egy k\u00E1rtyaj\u00E1t\u00E9kba gy\u00FArva. \u00DCljetek k\u00F6rbe, sorban h\u00FAzzatok k\u00E1rty\u00E1t \u00E9s k\u00F6vess\u00E9tek az utas\u00EDt\u00E1sokat!\r\n\r\n\tA j\u00E1t\u00E9k addig tart, am\u00EDg a piak\u00E9szlet. Az italt egys\u00E9gekben m\u00E9ritek. Ez lehet 1 dl s\u00F6r, egy korty bor  \u00E9s\\vagy 2 cl t\u00F6m\u00E9ny, a pia fajt\u00E1j\u00E1t szabadon ki lehet v\u00E1lasztani csak ne keverj\u00E9tek!\r\n\r\n\tMinden k\u00E1rtya, amit h\u00FAztok, egy feladatot jelent. Ez lehet iv\u00E1szat, k\u00E9rdezz-felelek vagy ak\u00E1rmi. Aki nem teljes\u00EDti, b\u00FCntet\u0151t kap (4 cl p\u00E1linka 10 m\u00E1sodpercig a sz\u00E1jban, esetleg egy ruha lev\u00E9tele).");
 		txtrInfo.setBounds(10, 11, 454, 319);
 		contentPane.add(txtrInfo);
 		
